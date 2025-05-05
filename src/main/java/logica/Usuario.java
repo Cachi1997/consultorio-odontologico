22e63abf63bfdaa80fcf9fdb8bab0e69d8e802d4
cachi1997 <cachigaliano1997@gmail.com>
2025-05-05 12:54:53 -0300

    first commit

---
+++ src/main/java/logica/Usuario.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+
+/**
+ *
+ * @author cachi
+ */
+public class Usuario {
+    private int id_usuario;
+    private String nombreUsuario;
+    private String contraseña;
+    private String rol;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id_usuario, String nombreUsuario, String contraseña, String rol) {
+        this.id_usuario = id_usuario;
+        this.nombreUsuario = nombreUsuario;
+        this.contraseña = contraseña;
+        this.rol = rol;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    
+    
+}
